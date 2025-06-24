6a78d26316d565a6483d28b28e8b5258855d87d6
GavinMcCormack912 <mccormackga@icloud.com>
2025-06-24 18:24:27 -0400

    useEffectPractice started

--- reactForms/src/components/StateLogin.jsx
+++ reactForms/src/components/StateLogin.jsx
@@ -1,16 +1,16 @@
-import { useRef } from 'react';
-// this approach uses refs! great.
+import { useState } from 'react';
+
 export default function Login() {
-  const email = useRef();
-  const password = useRef();
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [enteredPassword, setEnteredPassword] = useState("");
-    function handleSubmit(event) {
+     function handleSubmit(event) {
       event.preventDefault();
-      const enteredEmail = email.current.value;
-      const enteredPassword = password.current.value;
-      console.log(enteredEmail, enteredPassword, "it worked!")
-    }
+      console.log(enteredValues)
+     }
 
   // function handleEmailChange(event) {
   //     setEnteredEmail(event.target.value);
@@ -37,7 +37,8 @@ export default function Login() {
             id="email" 
             type="email" 
             name="email" 
-            ref={email}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
           />
         </div>
 
@@ -46,8 +47,9 @@ export default function Login() {
           <input 
             id="password" 
             type="password" 
-            name="password"
-            ref={password}
+            name="password" 
+            onChange={(event) => handleInputChange("password", event.target.value)}
+            value={enteredValues.password}
           />
         </div>
       </div>
